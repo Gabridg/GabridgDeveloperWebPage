57927e6bd6c06f0b2c95ed2c4c79b22cd0b9e59c
Gabridg <gabri99.del@gmail.com>
2024-04-16 15:25:47 +0200

    removed matrix effect

---
+++ database/connection.php
@@ -0,0 +1,8 @@
+<?php
+ // Connessione al database
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "gabriDeveloperDataBase";
+
+ $conn = new mysqli($servername, $username, $password, $dbname);
