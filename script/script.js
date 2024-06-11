5f052c7920148aaf79020df920b62d49bf394635
Gabridg <gabri99.del@gmail.com>
2024-06-11 10:16:31 +0200

    added structure to pages

--- script/script.js
+++ script/script.js
@@ -2,17 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
   const ics = document.getElementById('close-menu');
   const list = document.getElementById('burger-list');
   const burgerIcon = document.getElementById('burger-icon');
+  const main = document.getElementById('main');
 
 
   burgerIcon.addEventListener('click', function() {
-      list.classList.toggle('hidden');
-      ics.classList.remove('hidden');
-      burgerIcon.classList.add('hidden');
-  });
+    list.classList.remove('hidden');
+    list.classList.add('visible');
+    ics.classList.remove('hidden');
+    ics.classList.add('visible');
+    burgerIcon.classList.add('hidden');
+    burgerIcon.classList.remove('visible');
+    main.classList.remove('visible');
+    main.classList.add('hidden');
+});
 
-  ics.addEventListener('click', function() {
-    list.classList.toggle('hidden');
-    ics.classList.add('hidden');
-    burgerIcon.classList.remove('hidden');
+ics.addEventListener('click', function() {
+  list.classList.remove('visible');
+  list.classList.add('hidden');
+  ics.classList.remove('visible');
+  ics.classList.add('hidden');
+  burgerIcon.classList.remove('hidden');
+  burgerIcon.classList.add('visible');
+  main.classList.remove('hidden');
+  main.classList.add('visible');
 });
 });
